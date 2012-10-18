ca75f85c743be83d22453b425d070afe91d30af4
Celso Pitta <celso.pitta@gmail.com>
2012-10-18 12:03:52 -0300

    two rows

--- lib/zohoho/crm.rb
+++ lib/zohoho/crm.rb
@@ -38,6 +38,22 @@ module Zohoho
       record['Id']
     end
 
+    # converte lead
+    def convert_lead(lead_id, assign_to)
+      xmlData = parse_data({'createPotential' => false, 'assignTo' =>  assign_to, 'notifyLeadOwner' => true,
+                            'notifyNewEntityOwner'=> true }, 'Potentials')
+      id = @conn.call('Leads', 'convertLead', {:xmlData => xmlData, :newFormat => 1, :leadId=> lead_id }, :post)
+      id
+    end
+
+    # clicou em quero e preencheu formulario, virou cliente e adicionou proposta
+    def add_proposta_xml(xmlData)
+      #first_name, last_name = parse_name(name)
+      #xmlData = parse_data({'Potential Name' => first_name, "Stage" => "Closed Won", "Data de Fechamento" => "10/10/2012"  }, 'Potentials')
+      record = @conn.call('Potentials', 'insertRecords', {:xmlData => xmlData, :newFormat => 1}, :post)
+      record['Id']
+    end
+
    # lead eh criado quando usuario faz simulacao no site e fornece info de contato
     def add_lead(name)
       first_name, last_name = parse_name(name)
@@ -46,13 +62,6 @@ module Zohoho
      record['Id']
     end
 
-    # converte lead
-    def convert_lead(lead_id, assign_to)
-      xmlData = parse_data({'createPotential' => false, 'assignTo' =>  assign_to, 'notifyLeadOwner' => true,
-                            'notifyNewEntityOwner'=> true }, 'Potentials')
-     id = @conn.call('Leads', 'convertLead', {:xmlData => xmlData, :newFormat => 1, :leadId=> lead_id }, :post)
-     id
-    end
 
    # clicou em quero e preencheu formulario, virou cliente
    # NAO USAR, QUANDO CONVERTE LEAD ELE VIRA CLIENTE               
@@ -63,13 +72,6 @@ module Zohoho
      record['Id']
     end
 
-   # clicou em quero e preencheu formulario, virou cliente e adicionou proposta
-    def add_proposta(name)
-      first_name, last_name = parse_name(name)
-      xmlData = parse_data({'Potential Name' => first_name, "Stage" => "Closed Won", "Data de Fechamento" => "10/10/2012"  }, 'Potentials')
-     record = @conn.call('Potentials', 'insertRecords', {:xmlData => xmlData, :newFormat => 1}, :post)
-     record['Id']
-    end
 
     def post_note(entity_id, note_title, note_content)
       xmlData = parse_data({'entityId' => entity_id, 'Note Title' => note_title, 'Note Content' => note_content}, 'Notes')
@@ -84,8 +86,7 @@ module Zohoho
       XmlSimple.xml_out(data, :RootName => entry)
     end
 
-    private 
-    
+
     def parse_name(name)
       match_data = name.match(/\s(\S*)$/)
       match_data.nil? ? last_name = name : last_name = match_data[1]
@@ -94,7 +95,23 @@ module Zohoho
       match_data.nil? ? first_name = '' : first_name = match_data[1]
 
       return first_name, last_name
-    end 
+    end
+
+
+    def parse_data_2(row1, row2, entry)
+      fl1 = row1.map {|e| Hash['val', e[0], 'content', e[1]]}
+      row = Hash['no', '1', 'FL', fl1]
+      row1 = Hash['row', row]
+
+      fl2 = row2.map {|e| Hash['val', e[0], 'content', e[1]]}
+      row = Hash['no', '2', 'FL', fl2]
+      row2 = Hash['row', row]
+      data = row1 | row2
+      XmlSimple.xml_out(data, :RootName => entry)
+    end
+
+    private 
+
 
 
     def find_contacts_by_last_name(last_name)
